1e4e6997f6713112275dd82ca7102f3d6b9bf86a
nehavenkat <neha.veketsetty@gmail.com>
2019-12-11 18:30:26 +0100

    solved some errors

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const server = express(); // 2) creating new server
 const studentService = require("./src/services/student.js") // 12) we import our students router
 
 server.use(express.json()) //20)we specify that we are gonna handle the BODY of the requests as JSON for POST 
-server.listen(3100, () => { // 3)Running the server on the selected "port" and using Callback function
-    console.log("Server is running on 3100")
+server.listen(3200, () => { // 3)Running the server on the selected "port" and using Callback function
+    console.log("Server is running on 3200")
 });// 4) now run interminal nodemon server.js 5) check in "POSTMAN" on GET http://localhost:3100
 //6) now we need to creat first route for the server
 //---> Folder src -->Folder services --->file student.js and student.json
--- src/services/student.js
+++ src/services/student.js
@@ -5,7 +5,7 @@ const router = express.Router() // 9) we need a router "It will be the middlewar
 const fs = require("fs")
 const path = require("path")
 
-const studentsFilePath = path.join(__dirname, "students.json");// 16)define Directory
+const studentsFilePath = path.join(__dirname, "student.json");// 16)define Directory
 
 const readFile = () => { //17) create readfile function
     const buffer = fs.readFileSync(studentsFilePath)// read the students file path
@@ -17,18 +17,22 @@ const readFile = () => { //17) create readfile function
 //    res.send("hello")
 //})
 router.get('/',(req,res)=>{ //18) Now writing the 'Get' method and calling the readFile() and check in POSTMAN http://localhost:3100/students we get "[]"
-    res.send(readFile())
+const studentsArray = readFile()
+    res.send(studentsArray)
 })
 
 //19) now writing POST method to allow creating new students
 router.post('/',(req,res)=>{ 
-    var previousStudents = readFile(); //reads the students from the disk
-    req.body._id = previousStudents.length +1; //create a new id for the student
-    req.body.creationTime = new Date() //add the creationTime using new date()
 
-    previousStudents.push(req.body) //push the item into the students array
+    var previousStudents = readFile(); //reads the students from the disk
+    const student = {
+        ...req.body,
+        _id: previousStudents.length + 1,
+    }// create object first and then pysh it to previous array of students 
+
+    previousStudents.push(student) //push the item into the students array
     fs.writeFileSync(studentsFilePath, JSON.stringify(previousStudents)); //override the previous array on the harddrive
-    res.send({ _id: req.body._id }) //return the newly generated ID as object{}
+    res.send({ _id: student._id }) //return the newly generated ID as object{}
 })
 
 module.exports = router;// 10) Now we need to export the router
